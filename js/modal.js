7c070f18417bf978c1b560c4caf2b30f2846db3f
Martseniuk Alina <martseniuk.av@gmail.com>
2024-08-06 18:24:37 +0300

    add

--- js/modal.js
+++ js/modal.js
@@ -12,4 +12,35 @@
       refs.modal.classList.toggle("is-hidden");
       document.body.classList.toggle("no-scroll");
     }
-  })();
+})();
+  
+
+// (() => {
+//     const refs = {
+//       openModalBtn: document.querySelectorAll(".modal-open" ),
+//       closeModalBtn: document.querySelectorAll(".modal-close"),
+//       modal: document.querySelector("[data-modal]"),
+//   };
+//  for (let i = 0; i < refs.openModalBtn.length; i++) {
+// refs.openModalBtn[i].addEventListener("click", function() {
+//       refs.modal.classList.toggle("is-hidden");
+//       document.body.classList.toggle("no-scroll");
+//     })
+//   }
+//   for (let i = 0; i < refs.closeModalBtn.length; i++) {
+// refs.closeModalBtn[i].addEventListener("click", function() {
+//       refs.modal.classList.toggle("is-hidden");
+//       document.body.classList.toggle("no-scroll");
+//     })
+//  }
+    
+//   }
+  
+    // refs.openModalBtn.addEventListener("click", toggleModal);
+    // refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    // function toggleModal() {
+    //   refs.modal.classList.toggle("is-hidden");
+    //   document.body.classList.toggle("no-scroll");
+    // }
+  // })();
